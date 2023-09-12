d22421fb664f0d5bef5b659b575ec5367966ca42
oackland <oackland.toro@outlook.com>
2023-09-11 20:52:09 -0600

    'dates'

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,45 @@
+import datetime
+
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
-tracker = ["Test Habits",  "clean Room",  "eat Breakfast"]
+tracker = ["Test Habits", "clean Room", "eat Breakfast"]
+
+
+@app.context_processor
+def add_calc_date_range():
+	def date_range(start: datetime.date):
+		dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+		return dates
+
+	return {"date_range": date_range}
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", tracker=tracker, title="Home")
+	date_str = request.args.get("date")
+	if date_str:
+		selected_date = datetime.date.fromisoformat(date_str)
+	else:
+		selected_date = datetime.date.today()
+	return render_template(
+		"index.html", tracker=tracker, title="Home", 	selected_date=selected_date
+	)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
-    if request.form == "POST":
-        pass
-    return render_template("add_habit.html", title="Tracker")
+	if request.method == "POST":
+		tracker.append(request.form.get("habit"))
+	return render_template("add_habit.html", title="Tracker", selected_date=datetime.date.today(),
+						)
+
 
 
 @app.route("/fizzbuzz")
 def fizzbuzz():
-    return render_template("fizzbuzz.html", title="Fizzbuzz")
+	return render_template("fizzbuzz.html", title="Fizzbuzz")
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run(host='0.0.0.0', port=5001, debug=True)
